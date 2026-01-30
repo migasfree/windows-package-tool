65b440e3ffb355254e8d4c573ca732eda3e9162e
jact <jachavar@gmail.com>
2026-01-30 13:37:57 +0100

    chore: update copyright year and bump version to 1.1.0
    - Update copyright year from 2024 to 2024-2026
    - Bump version from 1.0.0 to 1.1.0

--- wpt/__init__.py
+++ wpt/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2024-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,4 +13,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
